7d30c9f39ecddb936ee1a0f2eca2ff75c7f6d34a
Nataliia Hirniak <gn.natalia19@gmail.com>
2023-10-16 08:21:05 +0100

    parametrized tests

--- src/test/utils.test.ts
+++ src/test/utils.test.ts
@@ -13,6 +13,17 @@ describe("Utils tests suite", () => {
     expect(actual).toBe(expected);
   });
 
+  describe.only("ToUpperCase Example", () => {
+    it.each([
+      { input: "abc", expected: "ABC" },
+      { input: "my-string", expected: "MY-STRING" },
+      { input: "bla", expected: "BLA" },
+    ])("$input toUpperCase should be $expected", ({ input, expected }) => {
+      const actual = toUpperCase(input);
+      expect(actual).toBe(expected);
+    });
+  });
+
   describe("getStringInfo for arg My-String should", () => {
     test("return right length", () => {
       const actual = getStringInfo("My-String");
