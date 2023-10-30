9f73aedb178381f884436ea9797e6f4f2fee68fb
Nataliia Hirniak <gn.natalia19@gmail.com>
2023-10-30 11:38:40 +0000

    Add own mock function

--- src/test/doubles/other-utils.test.ts
+++ src/test/doubles/other-utils.test.ts
@@ -4,6 +4,35 @@ import {
 } from "../../app/doubles/other-utils";
 
 describe("otherUtils test suite", () => {
+  describe.only("Tracking callbacks", () => {
+    let cbArg = [];
+    let timesCalled = 0;
+
+    function callbackMock(arg: string) {
+      cbArg.push(arg);
+      timesCalled++;
+    }
+
+    afterEach(() => {
+      //clearing tracking fields
+      cbArg = [];
+      timesCalled = 0;
+    });
+
+    it("calls callback for invalid argument - track calls", () => {
+      const actual = toUpperCaseWithCb("", callbackMock);
+      expect(actual).toBeUndefined();
+      expect(cbArg).toContain("Invalid argument");
+      expect(timesCalled).toBe(1);
+    });
+
+    it("calls callback for valid argument - track calls", () => {
+      const actual = toUpperCaseWithCb("abc", callbackMock);
+      expect(actual).toBe("ABC");
+      expect(cbArg).toContain(`call function with abc`);
+      expect(timesCalled).toBe(1);
+    });
+  });
   it("Calculate comlexity", () => {
     const someInfo = {
       length: 5,
